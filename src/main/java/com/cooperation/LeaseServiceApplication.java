0ac71fd971078e1c7b9a32e633b05158832d4c33
zhoukai <zhoukai@v.photos>
2018-06-06 10:46:49 +0800

    Initial commit

---
+++ src/main/java/com/cooperation/LeaseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cooperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaseServiceApplication.class, args);
+	}
+}
